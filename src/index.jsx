cdef83f4fa0121f567d0be59b0d2a72658335fd1
Yomium <yomuim@gmail.com>
2020-10-20 14:19:57 +0300

    Upload

---
+++ src/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import './css/index.css';
+import App from './components/App.jsx';
+import reducers from './reducers/index.js';
+
+const store = createStore(
+	reducers,
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
+
+render(
+	<Provider store={store} >
+		<App />
+	</Provider>,
+	document.getElementById('container'),
+);
